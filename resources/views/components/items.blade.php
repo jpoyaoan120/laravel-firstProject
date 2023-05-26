e7f4d5f8b2642c2cb2c7cc0cbce29feb9df172f0
Boss-JL <82464496+Boss-JL@users.noreply.github.com>
2023-05-26 13:47:07 +0800

    Updated

--- resources/views/components/items.blade.php
+++ resources/views/components/items.blade.php
@@ -1,9 +1,7 @@
 <ul class="flex flex-col md:flex-row">
+    @auth
     <li>
-        <a href="/login" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
-    </li>
-    <li>
-        <a href="/registration" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Register</a>
+        <a href="/add/student" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Add Student</a>
     </li>
     <li>
         <form action="/logout" method="POST">
@@ -11,4 +9,12 @@
             <button class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Logout</button>
         </form>
     </li>
+    @else
+    <li>
+        <a href="/login" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
+    </li>
+    <li>
+        <a href="/registration" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Register</a>
+    </li>
+    @endauth
 </ul>
